cd5801e1a6c4a3edc73ca12b7fde3076afcb98f5
Madhuri-Ramisetty <40954681+Madhuri-Ramisetty@users.noreply.github.com>
2018-07-14 16:46:20 +0530

    Arrayprint2.java

---
+++ javatraining/src/oca/javaarrays/ArrayPrint2.java
@@ -0,0 +1,13 @@
+package oca.javaarrays;
+import java.util.Arrays;
+public class ArrayPrint2 
+{
+	public static void main(String[] args)
+	{
+		int[] arr={1,4,13,5,6};
+		//printing elements of an array
+			System.out.println(Arrays.toString(arr));
+		
+	}
+
+}
