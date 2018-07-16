d029831f3af772ee50a55396d536fbf2ccc4277d
Madhuri-Ramisetty <40954681+Madhuri-Ramisetty@users.noreply.github.com>
2018-07-16 17:09:44 +0530

    Inherit

---
+++ javatraining/src/oca/javaclasses/InheritanceDemo.java
@@ -0,0 +1,35 @@
+package oca.javaclasses;
+
+class Add
+{
+	public int add(int i,int j)
+	{
+		return i+j;
+	}
+}
+class Sub extends Add
+{
+	public int sub(int i,int j)
+	{
+		return i-j;
+	}
+}
+class Mul extends Sub
+{
+	public int mult(int i,int j)
+	{
+		return i*j;
+	}
+}
+public class InheritanceDemo 
+{
+	public static void main(String[] args)
+	{
+		Mul s=new Mul();
+		int sum=s.add(13,4);
+		int dif=s.sub(22,13);
+		int mul=s.mult(31, 40);
+		System.out.println(sum+" "+dif+" "+mul);
+	}
+
+}
